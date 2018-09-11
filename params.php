fe97f61598984d07e93a4467e5272293ed766e1d
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2018-09-11 18:42:30 +0300

    upd .gitignore

--- params.php
+++ params.php
@@ -11,7 +11,7 @@ $gpxDir = 'gpx'; 	// track files directory, if present, in filesystem
 $tileCacheURI = '/tileproxy/tiles.php?z={z}&x={x}&y={y}&r={map}'; 	// uri of a map service, for example Galadriel tile cache/proxy service. In case GaladrielCache {map} is a map name in GaladrielCache app.
 //$tileCacheURI = '/tileproxy/tiles/{map}/{z}/{x}/{y}.{ext}'; 	//  uri of a map service - if used a local nginx or file system directly. If variable {ext} present in uri, extension of a tile file gets from GaladrielCache parms. So you must set $tileCachePath or define {ext} explicitly.
 
-$gpsanddataServerURI = 'askGPSD.php'; 	// uri of a active data service, if present. Commonly GNSS/AIS.
+//$gpsanddataServerURI = 'askGPSD.php'; 	// uri of a active data service, if present. Commonly GNSS/AIS.
 $currentTrackServerURI = 'getlasttrkpt.php'; 	// uri of a active track service, if present
 
 // Settings of a tile cache/proxy app
