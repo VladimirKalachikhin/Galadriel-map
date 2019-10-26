67063043f661558aeb52ad53ece3139f36962ff7
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2019-10-26 14:08:40 +0300

    bug fixed & bla-bla

--- askGPSD.php
+++ askGPSD.php
@@ -1,10 +1,12 @@
 <?php
 /* POLL your gpsd
 */
+ob_start(); 	// попробуем перехватить любой вывод скрипта
 $gpsdHost = 'localhost';
 $gpsdPort = 2947;
 require_once('fGPSD.php'); // fGPSD.php
 $LefletRealtime = posToLeafletRealtime($gpsdHost,$gpsdPort); 	// получим ВремяПозициюСкорость от gpsd
+ob_end_clean(); 			// очистим, если что попало в буфер
 header('Content-Type: application/json;charset=utf-8;');
 echo "$LefletRealtime \n";
 return;
