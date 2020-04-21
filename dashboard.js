ae41371d425912894596848c2cccd475f807cfdf
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2020-04-21 19:35:52 +0300

    forgotten files

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+var snd = new Audio("img/beep-02.wav");  
+function depthAlarm() {
+	setInterval(function(){snd.play();},300)
+}
+function maxSpeedAlarm() {
+	setInterval(function(){snd.play();},1000)
+}
+function minSpeedAlarm() {
+	setInterval(function(){snd.play();},1500)
+}
