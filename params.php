408e2dfc67ddc9b7b19ee6c8b3fca30edfb1f6c9
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2020-04-26 21:12:57 +0300

    first with AIS

--- params.php
+++ params.php
@@ -9,12 +9,12 @@ $trackDir = 'track'; 	// track files directory, if present, in filesystem
 $routeDir = 'route'; 	// route & POI files directory, if present, in filesystem
 
 // Services
-$tileCacheURI = '/tileproxy/tiles.php?z={z}&x={x}&y={y}&r={map}'; 	// uri of a map service, for example Galadriel tile cache/proxy service. In case GaladrielCache {map} is a map name in GaladrielCache app.
-//$tileCacheURI = 'http://a.tile.opentopomap.org/{z}/{x}/{y}.png'; 	//  uri of a map service - if no use GaladrielCache. Comment the $tileCachePath on this case.
+$tileCacheURI = '/tileproxy/tiles.php?z={z}&x={x}&y={y}&r={map}'; 	// uri of the map service, for example Galadriel tile cache/proxy service. In case GaladrielCache {map} is a map name in GaladrielCache app.
+//$tileCacheURI = 'http://a.tile.opentopomap.org/{z}/{x}/{y}.png'; 	//  uri of the map service - if no use GaladrielCache. Comment the $tileCachePath on this case.
 
-$gpsanddataServerURI = 'askGPSD.php'; 	// uri of a active data service, if present. Commonly GNSS/AIS.
-$currentTrackServerURI = 'getlasttrkpt.php'; 	// uri of a active track service, if present
-
+$gpsanddataServerURI = 'askGPSD.php'; 	// uri of the active data service, if present. Commonly spatial and vehicle data.
+$currentTrackServerURI = 'getlasttrkpt.php'; 	// uri of the active track service, if present
+$aisServerURI = 'askAIS.php'; 	// uri of the AIS data service, if present.
 // Settings of a tile cache/proxy app
 if( $tileCachePath) require("$tileCachePath/params.php"); 	// 
 ?>
